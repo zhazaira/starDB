ef9d40b017f21509b1fa4f3fcc9687d07450454d
zhazaira <zmahanzazira@gmail.com>
2024-04-20 02:00:37 +0500

    small changes

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 
 import './item-list.css'
-import SwapiService from "../../services/swapi-service";
+
 import Spinner from "../spinner";
 
 export default class ItemList extends Component {
-    swapiService = new SwapiService()
+
 
     state = {
-        peopleList: null
+        itemList: null
     }
 
     componentDidMount() {
-        this.swapiService
-            .getAllPeople()
-            .then((peopleList) => {
-                this.setState({ peopleList })
+
+        const { getData } = this.props
+
+        getData()
+            .then((itemList) => {
+                this.setState({ itemList })
             })
     }
 
@@ -33,12 +35,12 @@ export default class ItemList extends Component {
 
     render() {
 
-        const { peopleList } = this.state
+        const { itemList } = this.state
 
-        if (!peopleList) return <Spinner />
+        if (!itemList) return <Spinner />
 
-        const items = this.renderItems(peopleList)
-
+        const items = this.renderItems(itemList)
+        
         return (
             <ul className="item-list list-group">
                 { items }

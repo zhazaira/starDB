5723b2f4e90fb73319a07cd61af934e97f690378
zhazaira <zmahanzazira@gmail.com>
2024-04-19 09:11:18 +0500

    changr

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Spinner from '../spinner'
 import SwapiService from '../../services/swapi-service'
+import ErrorIndicator from "../error-indicator";
 
 import './random-planet.css'
 
@@ -11,7 +12,9 @@ export default class RandomPlanet extends Component {
 
     state = {
         planet: {},
-        loading: true
+        loading: true,
+        error: false
+    
     };
 
     constructor(props) {
@@ -26,20 +29,34 @@ export default class RandomPlanet extends Component {
         });
     };
 
+    
+    onError = (err) => {
+        this.setState({
+            loading: false,
+            error: true
+        });
+    }
+
     updatePlanet = () => {
         const id = Math.floor(Math.random()*15) + 2;
         this.swapiService
             .getPlanet(id)
-            .then(this.onPlanetLoaded);
+            .then(this.onPlanetLoaded)
+            .catch((err) => this.onError())
     }
 
     render() {
-        const { planet, loading } = this.state
+        const { planet, loading, error } = this.state
+
+        const hasData = !(loading || error)
+
+        const errorMessage = error ? <ErrorIndicator /> : null
         const spinner = loading ? <Spinner /> : null
-        const content = !loading ? <PlanetView planet={planet}/> : null
+        const content = hasData ? <PlanetView planet={planet}/> : null
 
         return (
             <div className="random-planet jumbotron rounded">
+                {errorMessage}
                 {spinner}
                 {content}
             </div>
@@ -49,8 +66,7 @@ export default class RandomPlanet extends Component {
 
 const PlanetView = ({ planet }) => {
 
-    const { id, name, population,
-        rotationPeriod, diameter } = planet;
+    const { id, name, population, rotationPeriod, diameter } = planet
 
     return (
         <React.Fragment>
@@ -74,7 +90,7 @@ const PlanetView = ({ planet }) => {
                 </ul>
             </div>
         </React.Fragment>
-    );
-};
+   )
+}
 
 

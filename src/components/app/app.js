fa9f5542843e50d3861f9941cded5d1bf76be748
zhazaira <zmahanzazira@gmail.com>
2024-04-26 02:08:58 +0500

    upd

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,12 @@
 import React, {Component} from 'react'
-
 import Header from '../header'
 import RandomPlanet from '../random-planet'
-
 import './app.css'
 import { ErrorIndicator, NotFoundIndicator } from "../errors"
 import ErrorBoundary from "../error-boundary"
-// import DummySwapiService from "../../services/dummy-swapi-service"
-
 import { SwapiServiceProvider } from '../swapi-service-context'
 import SwapiService from "../../services/swapi-service"
 import {PeoplePage, PlanetsPage, StarshipsPage, LoginPage, SecretPage, WelcomePage} from "../pages"
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import StarshipDetails from "../sw-components/starship-details";
 
@@ -51,26 +46,20 @@ export default class App extends Component{
                             <RandomPlanet />
                             <Switch>
                                 <Route path="/" component={WelcomePage} exact />
-
                                 <Route path="/people/:id?" component={PeoplePage} exact/>
-
                                 <Route path="/planets/:id?" component={PlanetsPage} exact/>
-
                                 <Route path="/starships" component={StarshipsPage} exact/>
                                 <Route path="/starships/:id" render={({ match }) => {
                                     const { id } = match.params
                                     return <StarshipDetails itemId={id} />
                                 }}/>
-
                                 <Route path="/login" render={() => (
                                     <LoginPage isLoggedIn={ isLoggedIn }
                                                onLogin={() => this.onLogin()} />
                                 )} exact />
-
                                 <Route path="/secret" render={() => (
                                     <SecretPage isLoggedIn={ isLoggedIn }/>
                                 )} exact />
-
                                 <Route component={NotFoundIndicator}/>
                             </Switch>
                         </div>
